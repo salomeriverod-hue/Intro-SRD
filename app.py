1d214a572b5796f24db96266e521d6540f6d0712
salomeriverod-hue <salomeriverod@gmail.com>
2026-02-26 13:25:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,7 @@ import streamlit as st
 st.title ("Mi primera app")
 st.header ("Esta página está dedicada a mi")
 from PIL import Image
-image = Image.open ("
+image = Image.open ("samicalvo.jpeg")
+st.image(image, caption= "este es sami")
+texto=st.text_inpit("Ingresa texto", "texto inicial")
+
