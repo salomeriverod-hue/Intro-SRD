802601486514be724dfa96821191ed44a7c1be72
salomeriverod-hue <salomeriverod@gmail.com>
2026-02-26 13:29:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ st.header ("Esta página está dedicada a mi")
 from PIL import Image
 image = Image.open ("samicalvo.jpeg")
 st.image(image, caption= "este es sami")
-texto=st.text_inpit("Ingresa texto", "texto inicial")
+texto = st.text_input("Ingresa texto", "texto inicial")
 st.write("El texto que has escrito es", texto)
